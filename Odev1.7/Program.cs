51a716ee041dc0423c9c913893675c5823c2f865
gga282 <103218918+gga282@users.noreply.github.com>
2022-08-14 17:49:02 +0200

    Add files via upload

---
+++ Odev1.7/Program.cs
@@ -0,0 +1,17 @@
+﻿//Kullanıcıdan 10 adet sayı alan ve bu sayıların 50 den küçük olanlarının sayısını bulup ekrana yazan uygulamayı geliştirin
+
+int[] arr = new int[10];
+Console.WriteLine("10 tane sayi girin: ");
+for(int i=0;i<arr.Length;i++)
+{
+    Console.Write((i+1)+". sayiyi giriniz: ");
+    arr[i] = int.Parse(Console.ReadLine());
+
+}
+for(int i=0;i<arr.Length;i++)
+{
+    if (arr[i] < 50)
+    {
+        Console.WriteLine("Sayi["+(i+1)+"]"+arr[i] +"<50");
+    }
+}
---
+++ Odev1.8/Program.cs
@@ -0,0 +1,12 @@
+﻿//Kullanıcıdan kaç tane sayıyı toplamak istediğini alan sonrasında toplanacak sayı adedi kadar sayıyı kullanıcıdan tek tek alıp bu sayıların toplamını ekrana yazan programı geliştirin
+int sum = 0;
+Console.WriteLine("Kac tane sayiyi toplamak istiyorsunuz: ");
+int size = int.Parse(Console.ReadLine());
+
+for(int i=0;i<size;i++)
+{
+    Console.Write((i+1)+". sayiyi giriniz:");
+    int input=int.Parse(Console.ReadLine());
+    sum += input;
+}
+Console.WriteLine("Sayilarin toplami: " + sum);
