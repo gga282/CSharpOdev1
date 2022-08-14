05ed4726fc1d03c3acbcb1fd0e76db15f6f4589d
gga282 <103218918+gga282@users.noreply.github.com>
2022-08-14 17:35:34 +0200

    Add files via upload

---
+++ odev1.4/Program.cs
@@ -0,0 +1,9 @@
+﻿int sum = 0;
+Console.Write("Bir sayi girin: ");
+string input = Console.ReadLine();
+int size = Convert.ToInt32(input);
+for(int i=0;i<size;i++)
+{
+    sum += i;
+}
+Console.WriteLine(size+"'ya kadar olan sayilarin toplami: " + sum);
---
+++ odev1.5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Tek Cift sayi toplama
+//
+int even_sum = 0;
+int odd_sum = 0;
+Console.WriteLine("Sayi gir: ");
+int input = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < input; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("Sayi: " + i + " Cift");
+        even_sum += i;
+    }
+    else
+    {
+        Console.WriteLine("Sayi: " + i + " Tek");
+        odd_sum += i;
+
+    }
+
+}
+Console.WriteLine("Tek sayilarin toplami: " + odd_sum);
+Console.WriteLine("Cift sayilarin toplami: " + even_sum);
