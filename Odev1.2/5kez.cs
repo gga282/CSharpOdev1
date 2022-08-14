fbdf1ccbd22b677998a270123a7b11edb7fb5c4d
gga282 <103218918+gga282@users.noreply.github.com>
2022-08-14 17:32:42 +0200

    Add files via upload

---
+++ Odev1.2/5kez.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Lutfen deger giriniz");
+var input = Console.ReadLine();
+Console.Clear();
+for(int i=0;i<5;i++)
+{
+    Console.WriteLine(input);
+}
---
+++ Odev1.3/odev1.3.cs
@@ -0,0 +1,10 @@
+﻿int i = 0;
+int sum = 0;
+do
+{
+    sum += i;
+    i++;
+
+} while (i < 10);
+
+Console.WriteLine(sum);
